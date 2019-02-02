c6f314aad13ab639ad4585b2cbfae8d5bbd31841
alexandrejacquier <alexandre.jacquier.1993@gmail.com>
2019-02-02 18:16:41 +0100

    le serveur utilisera le port heroku

--- server/server.js
+++ server/server.js
@@ -268,7 +268,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log("SERVER IS RUNNING ON PORT " + port);
 });
