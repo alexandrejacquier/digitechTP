a49364487eac3db7cf9254f3d5787450705bb99d
alexandrejacquier <alexandre.jacquier.1993@gmail.com>
2019-02-02 17:57:48 +0100

    ajout du frontend/build

---
+++ frontend/build/precache-manifest.05e06fdaab152cce3a7c5af5e89e3578.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c4d1bc725fa43f6c8f0",
+    "url": "/static/js/main.6c4d1bc7.chunk.js"
+  },
+  {
+    "revision": "78af1e07b546dc60344e",
+    "url": "/static/js/1.78af1e07.chunk.js"
+  },
+  {
+    "revision": "7593508774ab2d80bc96157761746c64",
+    "url": "/index.html"
+  }
+];
